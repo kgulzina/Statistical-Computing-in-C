a298cfdf069eeab867680ba8cf9ea1f603e3f046
Gulzina Kuttubekova <kgulzina@iastate.edu>
2019-09-06 22:11:29 -0700

    Upload intro exercises

---
+++ intro/hellogk.c
@@ -0,0 +1,19 @@
+/** @file hello.c
+ * First demonstration program.
+ * @author Gulzina Kuttubekova (kgulzina@iastate.edu)
+ * @date Friday Jan 18 17:34 CST 2019
+ *
+ * This code prints the string "Hello World!" to the screen and exits.
+ * This code used puts() instead of printf()
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	puts("Hello World!\n");
+	return EXIT_SUCCESS;  // program exits with status 0
+}
+
+
